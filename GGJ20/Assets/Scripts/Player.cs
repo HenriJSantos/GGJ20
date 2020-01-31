4fe2a0a6eb8e2e12f7ee4ad6a45abfd14f8eb116
Pedro Esteves <up201705160@fe.up.pt>
2020-01-31 18:57:14 +0000

    Added player scripts. Moved .gitignore

---
+++ GGJ20/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Player : MonoBehaviour
+{
+    public GameObject player;
+
+    public float hor_val;
+    public string hor_key;
+
+    public float ver_val;
+    public string ver_key;
+
+    private void Start()
+    {
+        hor_val = 1;
+        ver_val = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        if (Input.GetKey(this.hor_key))
+        {
+            HorizontalMove(this.hor_val);
+        }
+        else if (Input.GetKeyDown(this.ver_key))
+        {
+            VerticalMove(this.ver_val);
+        }
+    }
+
+    protected void HorizontalMove(float value)
+    {
+        this.player.transform.Translate(new Vector3(value, 0, 0));
+    }
+
+    protected void VerticalMove(float value)
+    {
+        this.player.transform.Translate(new Vector3(0, value, 0));
+    }
+}
