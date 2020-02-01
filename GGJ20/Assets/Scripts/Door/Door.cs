d4b5f9ed09314f7ec62807dfa7d3aeee22e43631
Márcia Teixeira <marcia309@hotmail.com>
2020-02-01 12:33:01 +0000

    Moved door

--- GGJ20/Assets/Scripts/Door/Door.cs
+++ GGJ20/Assets/Scripts/Door/Door.cs
@@ -7,16 +7,28 @@ public class Door : MonoBehaviour
     public Sprite unlocked;
     public Sprite locked;
     public Sprite open;
+
+    private bool player1;
+    private bool player2;
+
+    private bool isLocked;
     
     // Start is called before the first frame update
     void Start()
     {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = locked;
         
+        player1 = false;
+        player2 = false;
+
+        isLocked = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    
     }
+
+    
 }
