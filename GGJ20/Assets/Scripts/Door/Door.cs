1f3c1158f92d60339c9414ec30d851f12cc98583
Márcia Teixeira <marcia309@hotmail.com>
2020-02-01 14:15:59 +0000

    Door unlocking and opening

--- GGJ20/Assets/Scripts/Door/Door.cs
+++ GGJ20/Assets/Scripts/Door/Door.cs
@@ -12,23 +12,66 @@ public class Door : MonoBehaviour
     private bool player2;
 
     private bool isLocked;
+    private bool isOpen;
     
     // Start is called before the first frame update
     void Start()
     {
+        isLocked = true;
+        isOpen = false;
+
         this.gameObject.GetComponent<SpriteRenderer>().sprite = locked;
         
         player1 = false;
         player2 = false;
-
-        isLocked = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        if(isLocked){
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = locked;
+        }
+        else if(isOpen){
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = open;
+        }
+        else{
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = unlocked;
+        }
     }
 
-    
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        
+        if (collision.gameObject.CompareTag("Player1"))
+        {
+            player1 = true;
+            isLocked = false;
+        }
+        if (collision.gameObject.CompareTag("Player2"))
+        {
+            player2 = true;
+        }
+
+        if(player1 && player2 && !isLocked){
+            isOpen = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player1"))
+        {
+            player1 = false;
+        }
+        if (collision.gameObject.CompareTag("Player2"))
+        {
+            player2 = false;
+        }
+        
+        isOpen = false;
+        
+    }
+
 }
