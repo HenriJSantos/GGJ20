92a69137b82b3bf178dab96138b34bbcbbfbadef
Pedro Esteves <up201705160@fe.up.pt>
2020-02-02 14:08:46 +0000

    Added menu

--- GGJ20/Assets/Scripts/Menu/Menu.cs
+++ GGJ20/Assets/Scripts/Menu/Menu.cs
@@ -5,6 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public static bool started = false;
+    public GameObject menu;
+    public GameObject canvas;
+
+    private void Start()
+    {
+        if (started && SceneManager.GetActiveScene().name == "Menu")
+        {
+            Animation anim = canvas.GetComponent<Animation>();
+            if(anim != null)
+            {
+                anim.enabled = false;
+            }
+
+            menu.SetActive(true);
+        }
+        else
+        {
+            started = true;
+        }
+    }
+
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
