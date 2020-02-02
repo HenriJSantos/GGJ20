9211f3cb6faca6378a1bea600f54d7e99e5a2491
Pedro Esteves <up201705160@fe.up.pt>
2020-02-02 15:49:38 +0000

    Added story scene

---
+++ GGJ20/Assets/Scripts/Story.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Story : MonoBehaviour
+{
+    public GameObject[] images;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < images.Length; i++)
+        {
+            StartCoroutine(ActiveImage(images[i], i * 7.5f));
+        }
+        Invoke("NextScene", 33);
+    }
+
+    IEnumerator ActiveImage(GameObject image, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        image.SetActive(true);
+    }
+
+    private void NextScene()
+    {
+        SceneManager.LoadScene("Level 0");
+    }
+}
